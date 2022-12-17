699eca6fcabd789984a2b2bdf8ba0bf5e0551182
DominikDFER <dd52976@fer.hr>
2022-12-17 08:24:57 +0100

    cntroller

--- controller.js
+++ controller.js
@@ -3,23 +3,31 @@ const queries = require('./queries');
 
 const getIgrice  = (req,res) => {
    pool.query(queries.getIgrice ,(error,results)=>{
-    if(error) throw error;
+    if(error) res.status(400).send("error");
     res.status(200).json(results.rows);
    })
 }
 
 const getIgriceById = (req,res) => {
+    var testString = req.params.id;
+let isnum = /^\d+$/.test(testString);
+            
+if(!isnum){
+  res.status(405).send("Id nesmije sadrzavati slova");
+} 
+else {
     const id = parseInt(req.params.id);
     pool.query(queries.getIgriceById, [id], (error,results) => {
-        if(error) throw error;
+         if(error) res.status(400).send("error");
         res.status(200).json(results.rows);
     })
+ }
 }
 
 const getIgriceByPublisher = (req,res) => {
     const publisher = req.params.publisher;
     pool.query(queries.getIgriceByPublisher, [publisher], (error,results) => {
-        if(error) throw error;
+         if(error) res.status(400).send("error");
         res.status(200).json(results.rows);
     })
 }
@@ -27,7 +35,7 @@ const getIgriceByPublisher = (req,res) => {
 const getIgriceByName = (req,res) => {
     const name = req.params.name;
     pool.query(queries.getIgriceByName, [name], (error,results) => {
-        if(error) throw error;
+        if(error) res.status(400).send("error");
         res.status(200).json(results.rows);
     })
 }
@@ -35,7 +43,7 @@ const getIgriceByName = (req,res) => {
 const getIgriceByReview = (req,res) => {
     const review = req.params.review;
     pool.query(queries.getIgriceByReview, [review], (error,results) => {
-        if(error) throw error;
+        if(error) res.status(400).send("error");
         res.status(200).json(results.rows);
     })
 }
@@ -45,43 +53,59 @@ const addIgrica =(req,res) => {
 
 pool.query(queries.checkId,[id],(error,results)=>{
     if(results.rows.length){
-        res.send("taj is vec postoji")
+        res.status(400).send("taj id vec postoji")
     }
 })
 
  pool.query(queries.addIgrica,[id,name, žanr, publisher, voice_chat, review, pg_raiting, price, singleplayer_multiplayer_or_both, time_to_complete], (error, results)=>{
-    if(error) throw error;
+     if(error) res.status(400).send("error");
     res.status(201).send("igrica uspjesno dodana");
  })
 }
 
 const deleteIgrica = (req,res) =>{
+var testString = req.params.id;
+let isnum = /^\d+$/.test(testString);
+            
+if(!isnum){
+  res.status(405).send("Id nesmije sadrzavati slova");
+} 
+else {
     const id = parseInt(req.params.id);
     pool.query(queries.getIgriceById,[id],(error,results)=> {
         if(!results.rows.length){
-            res.send("nepostoji igrica s tim id")
+            res.status(404).send("nepostoji igrica s tim id")
         }
 
         pool.query(queries.deleteIgrica, [id], (error,results)=>{
-         if(error) throw error;
+          if(error) res.status(400).send("error");
          res.status(200).send("igrica uspjesno obrisana");
         })
     });
+    }
 }
 
 const upadteIgrica = (req,res)=>{
+    var testString = req.params.id;
+let isnum = /^\d+$/.test(testString);
+            
+if(!isnum){
+  res.status(405).send("Id nesmije sadrzavati slova");
+} 
+else {
     const id = parseInt(req.params.id);
     const {name} = req.body;
     pool.query(queries.getIgriceById, [id],(error,results)=> {
         if(!results.rows.length){
-            res.send("nepostoji igrica s tim id")
+            res.status(404).send("nepostoji igrica s tim id")
         }
 
         pool.query(queries.updateIgrica, [name, id], (error,results)=>{
-            if(error) throw error;
+             if(error) res.status(400).send("error");
             res.status(200).send("Ime igrice uspjesno azurirano");
            })
     });
+ }
 }
 
 
