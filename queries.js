bae8bede3d5daea97874687f1a43626d24979779
DominikDFER <dd52976@fer.hr>
2022-12-22 13:37:25 +0100

    promjene

--- queries.js
+++ queries.js
@@ -6,7 +6,7 @@ const getIgriceByReview = "SELECT * FROM  popis_igrica where review = $1";
 const checkId = "Select i FROM popis_igrica i WHERE i.id = $1";
 const addIgrica = "INSERT into popis_igirca(id,name, žanr, publisher, voice_chat, review, pg_raiting, price, singleplayer_multiplayer_or_both, time_to_complete) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10)";
 const deleteIgrica = "DELETE FROM popis_igrica WHERE id = $1";
-const updateIgrica = "UPDATE popis_igrica SET name = $1 WHERE is = $2";
+const updateIgrica = "UPDATE popis_igrica SET name = $1 WHERE id = $2";
 
 module.exports ={
     getIgrice, getIgriceById,
