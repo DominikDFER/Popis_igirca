0dd1ef534c770c898ea7e6588d97653e5ca2d2d6
DominikDFER <dd52976@fer.hr>
2022-12-22 13:58:32 +0100

    promjene

--- controller.js
+++ controller.js
@@ -18,7 +18,9 @@ if(!isnum){
 else {
     const id = parseInt(req.params.id);
     pool.query(queries.getIgriceById, [id], (error,results) => {
-         if(error) res.status(400).send("error");
+        if(!results.rows.length){
+            res.status(404).send("nepostoji igrica s tim id")
+        }
         res.status(200).json(results.rows);
     })
  }
